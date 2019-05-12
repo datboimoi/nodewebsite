e756b723c18dd762f815711518abe2cfd249e44b
Moises Rios <mrios4@uncc.edu>
2019-05-12 03:11:05 -0400

    HTML page

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 
 const PORT = process.env.PORT || 8888
 // Creates a localhost on port 8888
 app.listen(PORT, () => console.log("Server started on port 8888"))
 // Sets 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.send(path.join(__dirname, "templates", "main.html"))
 })
 
