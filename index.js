f55783d5b9f7045dbc34b79d968974e14f790901
Moises Rios <mrios4@uncc.edu>
2019-05-12 03:16:24 -0400

    Grammar update

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Sets a static folder
+app.use(express.static(path.join(__dirname, "templates")));
 
-const PORT = process.env.PORT || 8888
+const PORT = process.env.PORT || 8888;
 // Creates a localhost on port 8888
-app.listen(PORT, () => console.log("Server started on port 8888"))
-// Sets 
-app.get("/", (req, res) => {
-    res.send(path.join(__dirname, "templates", "main.html"))
-})
+app.listen(PORT, () => console.log("Server started on port 8888"));
+
 
