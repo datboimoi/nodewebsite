569c1b59bc32d1ada9a209249dd9f183440c26b6
Moises Rios <mrios4@uncc.edu>
2019-05-12 02:39:52 -0400

    nodemon dev

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8888
-
+// Creates a localhost on port 8888
 app.listen(PORT, () => console.log("Server started on port 8888"))
-
+// Sets 
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
